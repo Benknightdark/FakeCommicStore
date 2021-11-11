02f0e1cc43aee7c46b430d7fccc773eb2fb5c573
ben <moon791017@gmail.com>
2021-11-11 09:42:30 +0800

    update variable

--- crawler_projects/crawler/spiders/commic_url.py
+++ crawler_projects/crawler/spiders/commic_url.py
@@ -19,8 +19,8 @@ class CommicUrlSpider(RedisSpider):
             'REDIS_CONNECTION_STRING'), port=os.getenv('REDIS_CONNECTION_PORT'), db=0)
         parsed_url = urlparse(response.url)
         root_url = response.url.split('?')[0]
-        rootFolderName = parse_qs(parsed_url.query)['rootFolderName'][0]
-        subFolderName = parse_qs(parsed_url.query)['subFolderName'][0]
+        root_folder_name = parse_qs(parsed_url.query)['rootFolderName'][0]
+        sub_folder_name = parse_qs(parsed_url.query)['subFolderName'][0]
         commic_res = response.text
         commic_root = BeautifulSoup(commic_res, 'lxml')
         max_page = commic_root.find(
@@ -28,7 +28,7 @@ class CommicUrlSpider(RedisSpider):
         max_page_option_value = max_page.find_all('option')
         new_url_array = []
         for v in max_page_option_value:
-            new_url = f"{root_url}-p-{v['value']}?index={v['value']}&rootFolderName={rootFolderName}&subFolderName={subFolderName}"
+            new_url = f"{root_url}-p-{v['value']}?index={v['value']}&rootFolderName={root_folder_name}&subFolderName={sub_folder_name}"
             logging.info(new_url)
             new_url_array.append(new_url)
             redis_client.lpush('chapter_url:start_urls', new_url)
