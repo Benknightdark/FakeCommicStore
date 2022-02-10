1191a597f7f45ba8a4e8fd3e7c097b0f43cfe8d3
ben <moon791017@gmail.com>
2022-02-10 12:00:10 +0800

    aaa

--- web_site/pages/utils/layout.tsx
+++ web_site/pages/utils/layout.tsx
@@ -242,6 +242,40 @@ const Layout: NextPage = ({ children }) => {
                         {children}
 
                     </div>
+                    <div className="sidebar bg-blue-800 text-blue-100 w-64 space-y-6 py-7 px-2
+                      inset-y-0 left-0 transform  
+                      absolute     
+                      -translate-x-full  
+                      2xl:hidden
+                      lg:hidden
+                      xl:hidden
+                      md:hidden       
+                      sm:relative
+                      sm:translate-x-0 
+                      transition duration-200 ease-in-out
+                      ">
+                        <a href="#" className="text-white flex items-center space-x-2 px-4">
+                            <svg className="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                    d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+                            </svg>
+                            <span className="text-2xl font-extrabold">Better Dev</span>
+                        </a>
+                        <nav>
+                            <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white">
+                                Home
+                            </a>
+                            <a href="" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white">
+                                About
+                            </a>
+                            <a href="" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white">
+                                Features
+                            </a>
+                            <a href="" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white">
+                                Pricing
+                            </a>
+                        </nav>
+                    </div>
                     <footer className="py-5 bg-gray-700 text-center text-white">
                         made by ben 😎
                     </footer>
@@ -256,6 +290,8 @@ const Layout: NextPage = ({ children }) => {
             >
                 <Drawler />
             </SwipeableDrawer> */}
+
+
         </SubTitleContext.Provider>
     );
 }
