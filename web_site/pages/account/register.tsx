c6014e3dedc773bb52d998f3ec9d16b5747ba07f
ben <moon791017@gmail.com>
2022-02-15 10:44:31 +0800

    ok

--- web_site/pages/account/register.tsx
+++ web_site/pages/account/register.tsx
@@ -100,7 +100,7 @@ const Register: NextPage = ({ csrfToken }: InferGetServerSidePropsType<typeof ge
                             bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 
                             dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
                             dark:focus:border-blue-500"   id="email"
-                                            {...register("password")} />
+                                            {...register("email")} />
                                         {errors.email?.message && <div className="text-red-600" role="alert">
                                             <p>{errors.email?.message}</p>
                                         </div>}
