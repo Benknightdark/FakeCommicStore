5990e22a48d9b7ee1a7fe36b07bfbbe4ce395d50
ben <moon791017@gmail.com>
2022-05-05 17:30:05 +0800

    prepare update

--- crawler_projects/crawler/spiders/commic_url_spider.py
+++ crawler_projects/crawler/spiders/commic_url_spider.py
@@ -21,15 +21,29 @@ class CommicUrlSpider(RedisSpider):
         root_url = response.url.split('?')[0]
         root_folder_name = parse_qs(parsed_url.query)['rootFolderName'][0]
         sub_folder_name = parse_qs(parsed_url.query)['subFolderName'][0]
+        channel_id = parse_qs(parsed_url.query)['id'][0]
         commic_res = response.text
         commic_root = BeautifulSoup(commic_res, 'lxml')
-        max_page = commic_root.find(
-            'select', attrs={"name": 'select1'})
-        max_page_option_value = max_page.find_all('option')
         new_url_array = []
-        for v in max_page_option_value:
-            new_url = f"{root_url}-p-{v['value']}?index={v['value']}&rootFolderName={root_folder_name}&subFolderName={sub_folder_name}"
-            logging.info(new_url)
-            new_url_array.append(new_url)
-            redis_client.lpush('chapter_url:start_urls', new_url)
+        if channel_id == "1":
+            max_page = commic_root.find(
+                'select', attrs={"name": 'select1'})
+            max_page_option_value = max_page.find_all('option')
+            
+            for v in max_page_option_value:
+                new_url = f"{root_url}-p-{v['value']}?index={v['value']}&rootFolderName={root_folder_name}&subFolderName={sub_folder_name}"
+                logging.info(new_url)
+                new_url_array.append(new_url)
+                redis_client.lpush('chapter_url:start_urls', new_url)
+        if channel_id=="2":
+            image_el=commic_root.find_all('div',attrs={'class':'center scramble-page'})
+            i=1
+            for im in image_el:
+                src=im.find('img')['src']
+                new_url=f"{im.find('img')['src']}?index={v['value']}&rootFolderName={root_folder_name}&subFolderName={sub_folder_name}"
+                new_url_array.append(new_url)
+                redis_client.lpush('chapter_url:start_urls', new_url)
+                i=i+1
+                print(new_url)
+            pass
         return {'data': new_url_array}
--- web_site/pages/api/chapter.ts
+++ web_site/pages/api/chapter.ts
@@ -7,7 +7,7 @@ import { csrTokenCheck } from '../../helpers/csr-token-helper';
 type Data = {
     title: string | undefined,
     link: string | undefined,
-    checked:boolean
+    checked: boolean
 }
 
 /**
@@ -21,21 +21,31 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data[]>
 ) {
-    await csrTokenCheck(req,res)
-
+    await csrTokenCheck(req, res)
+    const id: string = req.query['id'].toString();
     const url: string = req.query['url'].toString()
     const reqData = await fetch(url)
     const resData = await reqData.text()
     const $ = cheerio.load(resData);
-
     const data: Data[] = []
-    $('#play_1').find('ul').children('li').each((index, element) => {
+    if (id === "1") {
+        $('#play_1').find('ul').children('li').each((index, element) => {
 
-        data.push({
-            title: $(element).find('a').attr('title'),
-            link: $(element).find('a').attr('href'),
-            checked:false
+            data.push({
+                title: $(element).find('a').attr('title'),
+                link: $(element).find('a').attr('href'),
+                checked: false
+            })
         })
-    })
+    } else {
+        $($('.btn-toolbar')[0]).find('a').each((i, el) => {
+            data.push({
+                title: $(el).find('li').text(),
+                link: `https://18comic.org${$(el).attr('href')}`,
+                checked: false
+            })
+        })
+    }
+
     res.status(200).json(data)
 }
