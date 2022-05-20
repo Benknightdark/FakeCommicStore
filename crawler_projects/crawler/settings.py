e4cb12203b10c9b5c6813b0568daa6b84873abca
ben <moon791017@gmail.com>
2022-05-20 17:31:00 +0800

    test pipeline

--- crawler_projects/crawler/settings.py
+++ crawler_projects/crawler/settings.py
@@ -66,10 +66,10 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-# #    'crawler.pipelines.CrawlerPipeline': 300,
-#     'crawler.pipelines.JsonWriterPipeline': 300,
-# }
+ITEM_PIPELINES = {
+#    'crawler.pipelines.CrawlerPipeline': 300,
+    'crawler.pipelines.JsonWriterPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
