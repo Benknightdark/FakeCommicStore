c878e8486ec358cb23b0cfb30e512a5217bc415f
ben <moon791017@gmail.com>
2022-05-31 10:36:20 +0800

    test

--- web_site/pages/_app.tsx
+++ web_site/pages/_app.tsx
@@ -35,7 +35,8 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  return getLayout(<SessionProvider session={pageProps.session} basePath={"/api/no/body/know"}>
+  return getLayout(
+  <SessionProvider session={pageProps.session} basePath={"/api/no/body/know"}>
     <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -44,5 +45,6 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  </SessionProvider>)
+  </SessionProvider>
+  )
 }
