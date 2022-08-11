94bd5857c822213b044b1cb3e37aed08a41b7d85
ben <moon791017@gmail.com>
2022-08-11 16:06:35 +0800

    aa

--- web_site/pages/api/view.ts
+++ web_site/pages/api/view.ts
@@ -15,19 +15,31 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    await csrTokenCheck(req,res)
-    const selectData=req.body
+    await csrTokenCheck(req, res)
+    const selectData = req.body
     const $ = (await getCherrioData(selectData.url))
-    let data:any[]=[]
+    let data: any[] = []
     switch (selectData.id) {
+        case 1:
+            const maxPage=$("[name='select1']").children().length;
+            const $2 = await getCherrioData(`${selectData.url}`);
+            const firstPageImage=$2('#ComicPic').attr('src');
+            data.push({ src:firstPageImage })
+            const p= new URL(firstPageImage!)
+            let ImageFileName=p.pathname.split('/')[p.pathname.split('/').length-1];
+            const rootUrl=firstPageImage!.replace(ImageFileName,'')
+            for (let index = 1; index <= maxPage; index++) {
+                 data.push({ src:`${rootUrl}${Number(ImageFileName.split('.')[0])+index}.jpg`})
+            }
+            break;
         case 3:
-            const imgList=$('.lazy')
-            imgList.each((i,elem)=>{
+            const imgList = $('.lazy')
+            imgList.each((i, elem) => {
                 console.log(elem.attributes[1].value)
-                data.push({src:elem.attributes[1].value})
+                data.push({ src: elem.attributes[1].value })
             })
             break;
-    
+
         default:
             break;
     }
